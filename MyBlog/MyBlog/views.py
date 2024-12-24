2a7483accbedbf60ddb90a823aabda552a9268a4
안준혁 <anjunhyeog@anjunhyeog-ui-MacBookAir.local>
2024-12-24 19:43:01 +0900

    다이어리 html추가,diary와 home 경로 설정

--- MyBlog/MyBlog/views.py
+++ MyBlog/MyBlog/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def index(request) :
-    return render(request, "index.html")
+    return render(request , "index.html")
+
+def diary(request) :
+    return render(request , "diary.html")
